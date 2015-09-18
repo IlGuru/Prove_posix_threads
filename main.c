77fca2303d6e69f5001201fcfa05dceb897dc906
IlGuru <s.busnelli@tin.it>
2015-09-18 16:11:30 +0200

    2015-03-01 19:06

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-// gcc -o main.exe main_001.c -lpthead
+// gcc -o main.exe main_002.c -lpthread -lcurses
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <ncurses.h>
 
 #define uchar unsigned char
 
@@ -15,87 +16,27 @@ void *thFunz0(void *thParam) {
 	mem = (uchar *)thParam;
    
 	while (1) {
-		mem[0]++;
-		if ( mem[0] == 255 ) mem[0] = 0;
+		mem[0] = getch();
+		printf("%c",mem[0]);
 	}
 	
 	pthread_exit(NULL);
 }
 
-void *thFunz1(void *thParam) {
-	uchar *mem;
-	mem = (uchar *)thParam;
-   
-	while (1) {
-		mem[1]++;
-		if ( mem[1] == 255 ) mem[1] = 0;
-	}
-	
-	pthread_exit(NULL);
-}
-
-void *thFunz2(void *thParam) {
-	uchar *mem;
-	mem = (uchar *)thParam;
-   
-	while (1) {
-		if ( mem[0] > mem[1] ) {
-			mem[2] = 255;
-		} else {
-			mem[2] = 0;
-		}
-	}
-	
-	pthread_exit(NULL);
-}
-
-void *thFunz3(void *thParam) {
-	uchar *mem;
-	mem = (uchar *)thParam;
-   
-	while (1) {
-		if ( mem[0] > mem[1] ) {
-			mem[3] = 255;
-		} else {
-			mem[3] = 0;
-		}
-	}
-	
-	pthread_exit(NULL);
-}
-
-void *thFunz4(void *thParam) {
-	uchar *mem;
-	mem = (uchar *)thParam;
-   
-	while (1) {
-		if ( mem[2] != mem[3] ) mem[4]++;
-	}
-	
-	pthread_exit(NULL);
-}
 
 int main( int argc, uchar *argv[] ) {
 	int rc;
 
 	pthread_t th_0;
-	pthread_t th_1;
-	pthread_t th_2;
-	pthread_t th_3;
-	pthread_t th_4;
 	
 	rc = pthread_create(&th_0, NULL, thFunz0, (void *)memory);
-	rc = pthread_create(&th_1, NULL, thFunz1, (void *)memory);
-	rc = pthread_create(&th_2, NULL, thFunz2, (void *)memory);
-	rc = pthread_create(&th_3, NULL, thFunz3, (void *)memory);
-	rc = pthread_create(&th_4, NULL, thFunz4, (void *)memory);
 	
 	while (1) {
-		printf( "memory: %08X ", memory );
-		for ( rc=0; rc<8; rc ++ ) {
-			printf( "%02X ",memory[rc] );
-		}
-		printf( "\r" );
+		// printf( "memory: %08X ", memory );
+		// for ( rc=0; rc<8; rc ++ ) {
+			// printf( "%02X ",memory[rc] );
+		// }
+		// printf( "\r" );
 	}
 	
 	pthread_exit(NULL);
